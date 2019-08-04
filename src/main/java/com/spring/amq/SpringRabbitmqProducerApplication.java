b5f217a290af86a1ffb280ebaadc712617787d73
sampath_inova <chaminda.sampath@inovaitsys.com>
2019-08-04 14:15:44 +0530

    initial commit rabbitmq consumer acknowledgement

---
+++ src/main/java/com/spring/amq/SpringRabbitmqProducerApplication.java
@@ -0,0 +1,11 @@
+package com.spring.amq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqProducerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqProducerApplication.class, args);
+	}
+}
